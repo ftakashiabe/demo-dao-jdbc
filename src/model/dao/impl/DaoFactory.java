24f70e617c81af5264f4981a165c3c5d696dd27d
ftakashiabe <takashi@ts2shift.com>
2020-05-12 13:32:47 -0300

    findById implementation

--- src/model/dao/impl/DaoFactory.java
+++ src/model/dao/impl/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao.impl;
 
+import db.DB;
 import model.dao.SellerDao;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJdbc();
+		return new SellerDaoJdbc(DB.getConnection());
 		
 	}
 	
