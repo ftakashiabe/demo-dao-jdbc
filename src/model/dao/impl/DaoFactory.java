ed23189df6dd8e00603a451cfbf65c79cc6406ca
ftakashiabe <takashi@ts2shift.com>
2020-05-12 23:14:32 -0300

    DepartmentDao implementation and test

--- src/model/dao/impl/DaoFactory.java
+++ src/model/dao/impl/DaoFactory.java
@@ -1,13 +1,16 @@
 package model.dao.impl;
 
 import db.DB;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJdbc(DB.getConnection());
-		
 	}
 	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJdbc(DB.getConnection());
+	}
 }
